a976a3608b5ac45f4116e2af9352416638664b3e
Tiago Pavani <tiagopavanisalto@hotmail.com>
2023-01-20 18:27:35 -0300

    add buttons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom'
+
 export const Header = () => {
   return (
     <header className='flex w-full h-full bg-neutral-900 relative'>  
-            <img className='w-full brightness-50 blur-sm mt-1' src="/src/assets/bgImg.jpg" />
-            <div className='text-white absolute flex flex-col items-center h-[24rem] w-[38rem] justify-center ml-48 mt-[10rem]'>
+            <img className='w-full brightness-50 blur-sm mt-1 ' src="/src/assets/bgImg.jpg" />
+            <div className='text-white absolute flex flex-col items-center h-[26rem] w-[36rem] justify-center ml-48 mt-[10rem]'>
               <h2 className='text-7xl mb-8'>Studio Alessandra Pavani</h2>    
               <p className='text-2xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non eius labore ut
               quibusdam culpa? Earum optio accusantium magni aliquid 
               laboriosam saepe iste dicta aperiam, minus quia culpa tempore fuga nesciunt?
               </p>
+              <Link className='text-white mt-4 flex self-end bg-neutral-800 w-32 h-12 items-center justify-center rounded-lg transition ease-in-out hover:-translate-y-1 hover:scale-x-105 duration-200 hover:bg-zinc-900'>Saiba mais</Link>
             </div>
-            
     </header>
   )
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom'
 
 export const Nav = () => {
   return (
-    <nav className='flex flex-row w-full h-24 bg-[#1B191A] items-center pb-2 justify-around overflow-hidden'>
+    <nav className='flex flex-row w-full h-24 bg-[#1B191A] items-center pb-2 justify-around overflow-hidden '>
             <img className='w-[9rem] mt-2' src="/src/assets/logo.jpg" alt="" />
-        <div className='flex flex-row justify-around w-64 '>
+          <div className='flex gap-12 pt-2'>
             <Link
             className='text-gray-500 text-xl hover:text-gray-200 transition-colors'
             >Home
@@ -20,7 +20,9 @@ export const Nav = () => {
             className='text-gray-500 text-xl hover:text-gray-200 transition-colors'
             >Sobre
             </Link>
-        </div>
+          </div>
+            <button className='w-32 h-10 mt-2 rounded text-lg text-gray-500 border-solid border-2 border-gray-500 
+            hover:text-gray-200 hover:border-gray-200 transition ease-in-out hover:scale-y-105 hover:scale-x-105 duration-200 '>Contate-nos</button>
     </nav>
   )
 }
